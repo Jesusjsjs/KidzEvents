374a8a67d7ad9437ac4b9ef33acb1500d472b651
Jesusjs Montejo <hertjhest@gmail.com>
2023-06-20 21:24:38 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kidz Events</title>
+
+    <link rel="stylesheet" href="./src/styles/home.css" />
+    <link rel="icon" type="image/png" href="./src/img/logoKidzEvents.png">
+
+
+</head>
+<body>
+    
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Crear</li>
+                    <li>Favoritos</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- Hero -->
+
+    <div class="hero">
+        <div class="infoHero" >
+            <div class="infoHeroChild"> 
+                <img class="imgInfoHero" alt="logo-kidz-events" src="./src/img/logoHero.png">
+                <h2>Busca, crea e inscribete a <b>eventos escolares </b></h2>
+                <div>
+                    <button class="buttonBlue">Ver eventos</button> 
+                    <button class="buttonPink">Inscribirse</button>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Imagenes right -->
+        <div class="imgsHeroRight">
+            <img src="./src/img/kidHeroBg.jpg" class="imagenHeroRight"  >
+            <img src="./src/img/girlHeroBg.jpg"  class="imagenHeroRight" >
+        </div>
+
+    </div>
+    <!-- Final del hero -->
+
+
+
+    <div class="events">
+    
+        <div class="eventsContainer">
+            
+            <!-- Card ejemplo -->
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+            
+            <div class="cardEvento">
+                <img src="./src/img/provDB/imgProvDB.jpg" class="imgCardEvento"/>
+                <div class="cardEventoInfo">
+                    <h5>Name evento hasta 5 chars..</h5>
+                    <div class="dateInfo">
+                        <h5>date</h5><h5>hour</h5>
+                    </div>
+                    <button>Ver evento</button>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="containerSearch">
+
+        </div>
+
+    </div>
+
+
+    <footer>
+        <div class="footerChild">
+            <img src="./src/img/logoColors.png" />
+            <div>
+                <h3>About us</h3> 
+                <h3>FQA</h3> 
+                <h3>Support</h3> 
+            </div>
+            <div>
+                <h3>Crear</h3> 
+                <h3>Favorito</h3> 
+                <h3>Inicio</h3> 
+            </div>
+            <div>
+                <h3>Facebook</h3> 
+                <h3>Instagram</h3> 
+                <h3>Whatsap</h3> 
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
